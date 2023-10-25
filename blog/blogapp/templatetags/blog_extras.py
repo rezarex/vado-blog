289f41ef534e9a43e51e9bc5d43e3b0eaaab9d9b
rezarex <marvin.wekesa@gmail.com>
2023-10-25 16:07:18 +0300

    FEATURE: added markdown support

--- blog/blogapp/templatetags/blog_extras.py
+++ blog/blogapp/templatetags/blog_extras.py
@@ -6,5 +6,6 @@ from django.template.defaultfilters import stringfilter
 register = template.Library()
 
 @register.filter
+@stringfilter
 def convert_markdown(value):
     return markdown.markdown(value, extensions = ['markdown.extensions.fenced_code'])
